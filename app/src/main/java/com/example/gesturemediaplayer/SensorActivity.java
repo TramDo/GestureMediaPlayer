5a80d3e1e7a52e4e5a6b61652df1d5c753a29b34
YunshanJiang <yunshantree@163.com>
2020-04-04 19:00:14 -0400

    basic sensor function

--- app/src/main/java/com/example/gesturemediaplayer/SensorActivity.java
+++ app/src/main/java/com/example/gesturemediaplayer/SensorActivity.java
@@ -2,21 +2,85 @@ package com.example.gesturemediaplayer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class SensorActivity extends Activity {
+public class SensorActivity extends Activity implements SensorEventListener {
 
+    final static String MYDEBUG = "MYDEBUG";
+    private SensorManager sensorManager;
+    private Sensor gyroSensor;
+    private  Sensor rotateSensor;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //Log.i(MYDEBUG, "onCreate! (setup)");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_layout);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        rotateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, rotateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     // Called when the "Exit" button is pressed.
     public void clickExit(View view) {
         this.finish(); // terminate
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+        {
+         //Log.i(MYDEBUG, String.valueOf(event.values[2]));
+        }
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
+            float[] rotationMatrix = new float[16];
+            SensorManager.getRotationMatrixFromVector(
+                    rotationMatrix, event.values);
+            // Remap coordinate system
+            float[] remappedRotationMatrix = new float[16];
+            SensorManager.remapCoordinateSystem(rotationMatrix,
+                    SensorManager.AXIS_X,
+                    SensorManager.AXIS_Z,
+                    remappedRotationMatrix);
+
+// Convert to orientations
+            float[] orientations = new float[3];
+            SensorManager.getOrientation(remappedRotationMatrix, orientations);
+
+            for(int i = 0; i < 3; i++) {
+                orientations[i] = (float)(Math.toDegrees(orientations[i]));
+                //Log.i(MYDEBUG, String.valueOf(orientations[i]));
+            }
+            Log.i(MYDEBUG, "X" + String.valueOf(orientations[0]));
+            Log.i(MYDEBUG, "Y" + String.valueOf(orientations[1]));
+            Log.i(MYDEBUG, "Z" +String.valueOf(orientations[2]));
+        }
+
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
