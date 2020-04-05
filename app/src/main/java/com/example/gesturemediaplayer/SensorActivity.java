758c7b0e06d38ff9a8f7d6d3462daae4bc44903e
YunshanJiang <yunshantree@163.com>
2020-04-05 00:06:04 -0400

    adjuect sencor

--- app/src/main/java/com/example/gesturemediaplayer/SensorActivity.java
+++ app/src/main/java/com/example/gesturemediaplayer/SensorActivity.java
@@ -17,11 +17,20 @@ public class SensorActivity extends Activity implements SensorEventListener {
 
     final static String MYDEBUG = "MYDEBUG";
 
+    final static int changedDegreeValue = 30;
+
     private MediaPlayer mediaPlayer;
     private SensorManager sensorManager;
     private Sensor gyroSensor;
     private  Sensor rotateSensor;
     private AudioManager audioManager;
+    private int maxvolume;
+    private int currentvolume;
+    private float currentRollValue;
+    private float currentPitchValue;
+    private float pitch;
+    private float roll;
+    private boolean enterActivity;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         //Log.i(MYDEBUG, "onCreate! (setup)");
@@ -33,12 +42,15 @@ public class SensorActivity extends Activity implements SensorEventListener {
         gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         rotateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
         audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+        maxvolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        currentvolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        Log.i(MYDEBUG, "max" +String.valueOf(maxvolume));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
+        enterActivity = false;
         sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(this, rotateSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -59,7 +71,7 @@ public class SensorActivity extends Activity implements SensorEventListener {
         {
             //   Log.i(MYDEBUG, "X" + String.valueOf(event.values[0]));
 //            Log.i(MYDEBUG, "Y" + String.valueOf(event.values[1]));
-            Log.i(MYDEBUG, "Z" +String.valueOf(event.values[2]));
+          //  Log.i(MYDEBUG, "Z" +String.valueOf(event.values[2]));
         }
         if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
             float[] rotationMatrix = new float[9];
@@ -70,31 +82,43 @@ public class SensorActivity extends Activity implements SensorEventListener {
             SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisX, worldAxisZ, adjustedRotationMatrix);
             float[] orientation = new float[3];
             SensorManager.getOrientation(adjustedRotationMatrix, orientation);
-            float pitch = orientation[1] * -57;
-            float roll = orientation[2] * -57;
+            pitch = orientation[1] * -57;
+            roll = orientation[2] * -57;
             // Log.i(MYDEBUG, "X" + String.valueOf(pitch));
 //            Log.i(MYDEBUG, "Y" + String.valueOf(orientations[1]));
-            Log.i(MYDEBUG, "Z" +String.valueOf(roll));
-            if (pitch >= 45.0f){
+           // Log.i(MYDEBUG, "Z" +String.valueOf(roll));
+            if (!enterActivity){
+                enterActivity = !enterActivity;
+                currentPitchValue = pitch;
+                currentRollValue = roll;
+            }
+            if (pitch >= 45.0f && Math.abs(pitch - currentPitchValue) >= changedDegreeValue){
                 // mediaPlayer.start();
-                Log.i(MYDEBUG, "X" + String.valueOf(pitch));
+              //  Log.i(MYDEBUG, "X" + String.valueOf(pitch));
                 mediaPlayer.pause();
             }
-            else if (pitch <= -45.0f){
+            else if (pitch <= -45.0f && Math.abs(pitch - currentPitchValue) >= changedDegreeValue){
                 // mediaPlayer.pause();
-                Log.i(MYDEBUG, "X" + String.valueOf(pitch));
+                //Log.i(MYDEBUG, "X" + String.valueOf(pitch));
                 mediaPlayer.start();
             }
-            if (roll <= -45.0f){
-                  audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+            if (roll <= -45.0f && Math.abs(roll - currentRollValue) >= changedDegreeValue){
+                 // audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
                // mediaPlayer.start();
+                Log.i(MYDEBUG, "Z" +String.valueOf(roll));
+                currentvolume += 1;
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentvolume, AudioManager.FLAG_SHOW_UI);
             }
-            else if(roll >= 45.0f ){
-                  audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
+            else if(roll >= 45.0f  && Math.abs(roll - currentRollValue) >= changedDegreeValue){
+                //  audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
                // mediaPlayer.pause();
+                Log.i(MYDEBUG, "Z" +String.valueOf(roll));
+                currentvolume -= 1;
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentvolume, AudioManager.FLAG_SHOW_UI);
             }
         }
-
+        currentRollValue = roll;
+        currentPitchValue = pitch;
     }
 
 
